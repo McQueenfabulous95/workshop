f6f40801446648df47e1625b274045b04002d9bd
Manikanta <manikantabunny412@gmail.com>
2024-03-04 10:12:06 +0530

    adding

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+number = int(input("enter a number: "))
+sum = 0
+temp == number
+while number>0 :
+    r = number%10
+    sum = sum+r*r*r
+    number = number//10
+    if temp==sum :
+        print("is an armstrong")
+    else :
+        print("not an armstrong")   
+
